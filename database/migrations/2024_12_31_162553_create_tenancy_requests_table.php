cc297758af80be66ec68e35b5ed217d61781ba1d
nn-287 <100829062+nn-287@users.noreply.github.com>
2024-12-31 19:49:39 +0200

    Rename tenancyrequest table to tenancy_requests and add sessions migration

--- database/migrations/2024_12_31_162553_create_tenancy_requests_table.php
+++ database/migrations/2024_12_31_162553_create_tenancy_requests_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tenancyrequest', function (Blueprint $table) {
+        Schema::create('tenancy_requests', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
