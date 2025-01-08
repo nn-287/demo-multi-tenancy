8e0240321ab20903213a50973098b03f0d7e885b
nn-287 <100829062+nn-287@users.noreply.github.com>
2025-01-08 14:45:59 +0200

    Remove commented-out error handling in AdminLoginForm login method

--- app/Livewire/AdminLoginForm.php
+++ app/Livewire/AdminLoginForm.php
@@ -19,23 +19,17 @@ class AdminLoginForm extends Component
 
     public function login()
     {
-        //TODO - Add toaster message
        $validatedData = $this->validate();
         if($validatedData){
 
             if (Auth::attempt(['email' => $validatedData['email'], 'password' => $validatedData['password']])) {
                 return redirect('admin-dashboard');
             }
-            // session()->flash('error', 'Login failed. Please check your email and password.');
             return redirect('/');
         }
-
-        // session()->flash('error', 'There was an issue with the data you provided. Please try again.');
         return redirect('/');
-
-        
-        
     }
+    
     public function render()
     {
         return view('livewire.admin-login-form');
