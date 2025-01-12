8dab294188d8d7d282bc9b7630d7fb98b0a27da0
nn-287 <100829062+nn-287@users.noreply.github.com>
2025-01-12 15:53:17 +0200

    Add route for listing courses in user dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
 use App\Livewire\AdminLoginForm;
 use App\Livewire\AdminPanel;
+use App\Livewire\ListCourse;
 use App\Livewire\TenancyRequest;
 use App\Livewire\UserLoginForm;
 use App\Livewire\UserPanel;
@@ -14,11 +14,14 @@ Route::get('/tenancyrequest', TenancyRequest::class)->name('tenancyrequest');
 Route::get('/login', AdminLoginForm::class)->name('login');
 Route::get('/user-login', UserLoginForm::class)->name('user-login');
 
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/admin-dashboard', AdminPanel::class)->name('admin-dashboard');   
 });
 
 Route::middleware(['auth'])->group(function () {
     //Tenant users simulation
-    Route::get('/user-dashboard', UserPanel::class)->name('user-dashboard');   
+    Route::get('/user-dashboard', UserPanel::class)->name('user-dashboard');  
+    Route::get('/list-courses', ListCourse::class);  
+
 });
