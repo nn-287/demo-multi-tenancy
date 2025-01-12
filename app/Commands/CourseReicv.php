03bc35e5cfc96b93b0c11eee27b45c4a67e73b0c
nn-287 <100829062+nn-287@users.noreply.github.com>
2025-01-12 15:50:00 +0200

    Add command pattern implementation

---
+++ app/Commands/CourseReicv.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Commands;
+
+use App\Models\Course;
+use App\Models\CourseUser;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class CourseReicv
+{
+    // public $courses = [];
+    // public function listCourses():array
+    // {
+    //     // $userId = auth()->id();  
+    //     // // dd($userId);
+    //     // $courses = CourseUser::where('user_id', $userId)->get()->pluck('course')->all();  
+
+    //     // dd($courses);
+
+    //     // // return $courses;
+    //     return Course::all()->toArray();
+    // }
+}
---
+++ app/Commands/ListCoursesCommand.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Commands;
+use App\Models\Course;
+
+// class ListCoursesCommand implements Command
+// {
+//     private CourseReicv $courseReceiver;
+
+//     public function __construct(CourseReicv $courseReceiver)
+//     {
+//         $this->courseReceiver = $courseReceiver;
+//     }
+
+//     // public function execute(): array
+//     // {
+//     //     return $this->courseReceiver->listCourses();
+//     // }
+// }
