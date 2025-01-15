9144d4c119c698ed6757b321d3cc8df59afda739
nn-287 <100829062+nn-287@users.noreply.github.com>
2025-01-15 21:35:39 +0200

    Add route for BTest component
    Clean up course listing route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
+
+use App\Http\Controllers\BBBController;
 use App\Livewire\AdminLoginForm;
 use App\Livewire\AdminPanel;
+use App\Livewire\BTest;
 use App\Livewire\CourseComponent;
 use App\Livewire\ListCourse;
+use App\Livewire\Schedule;
 use App\Livewire\TenancyRequest;
 use App\Livewire\UserLoginForm;
 use App\Livewire\UserPanel;
@@ -14,7 +18,7 @@ Route::get('/', function () {
 Route::get('/tenancyrequest', TenancyRequest::class)->name('tenancyrequest');
 Route::get('/login', AdminLoginForm::class)->name('login');
 Route::get('/user-login', UserLoginForm::class)->name('user-login');
-
+Route::get('/test',BTest::class)->name('test');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/admin-dashboard', AdminPanel::class)->name('admin-dashboard');   
@@ -23,6 +27,5 @@ Route::middleware(['auth'])->group(function () {
 Route::middleware(['auth'])->group(function () {
     //Tenant users simulation
     Route::get('/user-dashboard', UserPanel::class)->name('user-dashboard');  
-    //  Route::get('/list-courses', CourseComponent::class);  
 
 });
