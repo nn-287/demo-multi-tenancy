4ccb0fb093507d6a3f83cdbd9f064f402f39f032
nn-287 <100829062+nn-287@users.noreply.github.com>
2025-01-19 16:08:56 +0200

    Disable automatic refresh in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: false,
         }),
+
+        // livewire({
+        //     refresh: ['resources/css/app.css'],
+        // }),
     ],
 });
